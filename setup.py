4665813c11b6844e8baf0182bfc1f2660641d2e4
James Boulton <james@james.boulton@xtra.co.nz>
2020-06-12 20:11:16 +1200

    Selector now works in the menu.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(name='dashio',
-    version=1.21,
+    version=1.23,
     # py_modules=['iotcontrol', 'iotconnection'],
     description='Dashio interface library',
     long_description=long_description,
