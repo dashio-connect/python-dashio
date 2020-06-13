4ce5db12f51fe264fc6b8c3796fdab3b84bb0a68
James Boulton <james@james.boulton@xtra.co.nz>
2020-06-13 12:37:16 +1200

    Updates to the Selector Control

--- dashio/iotcontrol/selector.py
+++ dashio/iotcontrol/selector.py
@@ -10,7 +10,10 @@ class Selector(Control):
                  control_title='A Selector',
                  max_font_size=20,
                  text_align=TextAlignment.LEFT,
-                 sent_selection_colour=Colour.WHITE):
+                 sent_selection_colour=Colour.WHITE,
+                 background_colour=Colour.BLUE,
+                 text_colour=Colour.WHITE_SMOKE,
+                 title_bar_colour=Colour.RED):
         super().__init__('SLCTR', control_id)
         self.title = control_title
         self.max_font_size = max_font_size
@@ -18,6 +21,9 @@ class Selector(Control):
         self.sent_selection_colour = sent_selection_colour
         self.selection_list = []
         self._position = 0
+        self.background_colour = background_colour
+        self.text_colour = text_colour
+        self.title_bar_colour = title_bar_colour
         self._cfg['selection'] = self.selection_list
         
     def get_state(self):
@@ -70,3 +76,31 @@ class Selector(Control):
     def sent_selection_colour(self, val: Colour):
         self._sent_selection_colour = val
         self._cfg['sentSelectionColour'] = str(val.value)
+    
+    @property
+    def background_colour(self) -> Colour:
+        return self._background_colour
+
+    @background_colour.setter
+    def background_colour(self, val: Colour):
+        self.background_colour = val
+        self._cfg['backgroundColour'] = str(val.value)
+
+    @property
+    def text_colour(self) -> Colour:
+        return self.text_colour
+
+    @text_colour.setter
+    def text_colour(self, val: Colour):
+        self._text_colour = val
+        self._cfg['textColour'] = str(val.value)
+
+    
+    @property
+    def title_bar_colour(self) -> Colour:
+        return self._title_bar_colour
+
+    @title_bar_colour.setter
+    def title_bar_colour(self, val: Colour):
+        self._title_bar_colour = val
+        self._cfg['titleBarColour'] = str(val.value)
