b8e75303c0cda3212ec8b7f4db1701e07250d5bd
James Boulton <james.boulton@xtra.co.nz>
2020-07-15 14:12:49 +1200

    Added ButtonGroup control.

--- setup.py
+++ setup.py
@@ -4,23 +4,21 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(name='dashio',
-    version='1.31.4',
-    # py_modules=['iotcontrol', 'iotconnection'],
-    description='Dashio interface library',
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    author='James Boulton',
-    author_email='james.boulton@kotukublue.com',
-    url="https://github.com/dashio-connect/python-dashio",
-    # package_dir={'': 'dashio'},
-    packages=find_packages(),
-    license='MIT',
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "Operating System :: OS Independent"
-    ],
-    install_requires=[
-        'paho-mqtt'
-    ]
-
-    )
+      version='1.31.5',
+      # py_modules=['iotcontrol', 'iotconnection'],
+      description='Dashio interface library',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      author='James Boulton',
+      author_email='james.boulton@kotukublue.com',
+      url="https://github.com/dashio-connect/python-dashio",
+      # package_dir={'': 'dashio'},
+      packages=find_packages(),
+      license='MIT',
+      classifiers=[
+          "Programming Language :: Python :: 3",
+          "Operating System :: OS Independent"
+      ],
+      install_requires=[
+          'paho-mqtt'
+      ])
