cd032deee33bcda946a9cdcbe579ef2926749e89
James Boulton <james@james.boulton@xtra.co.nz>
2020-08-13 22:09:49 +1200

    Add name control.

--- dashio/__init__.py
+++ dashio/__init__.py
@@ -19,3 +19,4 @@ from .iotcontrol.page import Page
 from .iotcontrol.control import ControlPosition
 from .iotcontrol.button_group import ButtonGroup
 from .iotcontrol.event_log import EventData, EventLog
+# from .iotcontrol.name import Name
