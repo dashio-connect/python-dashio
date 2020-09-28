01de69c54f6ea99e37fdd7b222a82c161947216d
James Boulton <james.boulton@xtra.co.nz>
2020-09-29 11:01:18 +1300

    Work on TestTCP to find bug.

--- dashio/btconnection.py
+++ dashio/btconnection.py
@@ -1,11 +1,25 @@
-import gatt
 
 
-class AnyDeviceManager(gatt.DeviceManager):
-    def device_discovered(self, device):
-        print("Discovered [%s] %s" % (device.mac_address, device.alias()))
+#!/usr/bin/python3
+
+from gi.repository import GObject
+import os
+import spp_server
 
 
-manager = AnyDeviceManager(adapter_name="hci0")
-manager.start_discovery()
-manager.run()
+def my_read_cb(value):
+    print('my callback: {}'.format(value))
+
+
+def my_write_cb(my_server):
+    cpu_temp = os.popen('vcgencmd measure_temp').readline()
+    if my_server.fd_available():
+        my_server.write_spp('{}'.format(float(cpu_temp.replace('temp=', '').replace("'C\n", ''))))
+        print('Sending: {}'.format(float(cpu_temp.replace('temp=', '').replace("'C\n", ''))))
+    return True
+
+if __name__ == '__main__':
+    my_spp_server = spp_server.SPP(my_read_cb)
+    GObject.timeout_add(1000, my_write_cb, my_spp_server)
+    my_spp_server.start()
+
