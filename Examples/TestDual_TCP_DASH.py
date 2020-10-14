de64f3ecf8aad3f92c6e40199ec5fde0cc4a7761
James Boulton <james@james.boulton@xtra.co.nz>
2020-10-14 18:57:20 +1300

    Updated coords

--- Examples/TestDual_TCP_DASH.py
+++ Examples/TestDual_TCP_DASH.py
@@ -117,7 +117,7 @@ class TestControls:
         self.page_name = "TestTCP: " + platform.node()
 
         self.page_test = dashio.Page("TestTCP", self.page_name, 1)
-        self.up_btn = dashio.Button("UP_BTN", control_position=dashio.ControlPosition(0.05, 0.02857, 0.2, 0.114286))
+        self.up_btn = dashio.Button("UP_BTN", control_position=dashio.ControlPosition(0.04, 0.0099999, 0.2, 0.114286))
         self.up_btn.btn_state = dashio.ButtonState.OFF
         self.up_btn.icon_name = dashio.Icon.UP
         self.up_btn.on_colour = dashio.Colour.GREEN
@@ -128,7 +128,7 @@ class TestControls:
         self.page_test.add_control(self.up_btn)
 
         self.down_btn = dashio.Button(
-            "DOWN_BTN", control_position=dashio.ControlPosition(0.05, 0.771429, 0.2, 0.114286)
+            "DOWN_BTN", control_position=dashio.ControlPosition(0.04, 0.74999999, 0.2, 0.114286)
         )
         self.down_btn.btn_state = dashio.ButtonState.OFF
         self.down_btn.icon_name = dashio.Icon.DOWN
@@ -140,7 +140,7 @@ class TestControls:
         self.page_test.add_control(self.down_btn)
 
         self.sldr_cntrl = dashio.SliderSingleBar(
-            "SLDR", control_position=dashio.ControlPosition(0.05, 0.1428567, 0.2, 0.628571)
+            "SLDR", control_position=dashio.ControlPosition(0.04, 0.12, 0.2, 0.628571)
         )
         self.sldr_cntrl.title = "Slider"
         self.sldr_cntrl.max = 10
@@ -150,7 +150,7 @@ class TestControls:
         self.page_test.add_control(self.sldr_cntrl)
 
         self.sldr_dbl_cntrl = dashio.SliderDoubleBar(
-            "SLDR_DBL", control_position=dashio.ControlPosition(0.75, 0.028571, 0.2, 0.857143)
+            "SLDR_DBL", control_position=dashio.ControlPosition(0.76, 0.01, 0.2, 0.857143)
         )
         self.sldr_dbl_cntrl.title = "Slider Double"
         self.sldr_dbl_cntrl.max = 5
@@ -159,7 +159,7 @@ class TestControls:
         self.sldr_dbl_cntrl.message_rx_event += self.slider_dbl_event_handler
         self.page_test.add_control(self.sldr_dbl_cntrl)
 
-        self.knb_control = dashio.Knob("KNB", control_position=dashio.ControlPosition(0.25, 0.171429, 0.5, 0.2))
+        self.knb_control = dashio.Knob("KNB", control_position=dashio.ControlPosition(0.24, 0.15, 0.52, 0.2))
         self.knb_control.title = "A Knob"
         self.knb_control.max = 10
         self.knb_control.red_value = 10
@@ -172,7 +172,7 @@ class TestControls:
         self.page_test.add_control(self.dl_control)
 
         self.text_cntrl = dashio.TextBox(
-            "TXT1", control_position=dashio.ControlPosition(0.25, 0.771429, 0.5, 0.114286)
+            "TXT1", control_position=dashio.ControlPosition(0.25, 0.75, 0.52, 0.11)
         )
         self.text_cntrl.text = "Hello"
         self.text_cntrl.title = "A text control"
@@ -183,12 +183,12 @@ class TestControls:
 
         self.alarm_ctrl = dashio.Alarm("TestingAlarms", "Test Alarms", "Hello", "Test of Shared Alarms")
 
-        self.comp_control = dashio.Compass("COMP1", control_position=dashio.ControlPosition(0.25, 0.371429, 0.5, 0.2))
+        self.comp_control = dashio.Compass("COMP1", control_position=dashio.ControlPosition(0.24, 0.35, 0.52, 0.2))
         self.comp_control.title = "A compass"
         self.page_test.add_control(self.comp_control)
 
         self.selector_ctrl = dashio.Selector(
-            "TestSelector", "A Selector", control_position=dashio.ControlPosition(0.25, 0.028571, 0.5, 0.142857)
+            "TestSelector", "A Selector", control_position=dashio.ControlPosition(0.25, 0.001, 0.52, 0.14)
         )
         self.selector_ctrl.message_rx_event += self.selector_ctrl_handler
         self.selector_ctrl.add_selection("First")
@@ -205,7 +205,7 @@ class TestControls:
             text="Hello from Label",
             style=dashio.LabelStyle.GROUP,
             colour=dashio.Colour.BLUE,
-            control_position=dashio.ControlPosition(0.0, 0.0, 1.0, 0.914286),
+            control_position=dashio.ControlPosition(0.0, 0.0, 1.0, 0.9),
         )
         self.page_test.add_control(self.label_ctrl)
 
