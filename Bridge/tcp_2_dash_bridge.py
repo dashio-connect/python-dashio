95851dbfa901fcca17786dc739399b24798cb874
James Boulton <james.boulton@xtra.co.nz>
2020-10-23 16:49:21 +1300

    Work on tcp bridge

--- Bridge/tcp_2_dash_bridge.py
+++ Bridge/tcp_2_dash_bridge.py
@@ -18,7 +18,7 @@ class ZeroConfDashTCPListener:
     def __init__(self, context=None):
         self.context = context or zmq.Context.instance()
         self.zmq_socket = self.context.socket(zmq.PUSH)
-        self.zmq_socket.bind("inproc://zconf")
+        self.zmq_socket.connect("inproc://zconf")
 
     def remove_service(self, zeroconf, type, name):
         info = zeroconf.get_service_info(type, name)
@@ -38,7 +38,13 @@ class ZeroConfDashTCPListener:
 
 class TCPPoller(threading.Thread):
 
-    def check_port(self, ip, port):
+    def check_port(self, ip, port, context):
+
+        self.context = context or zmq.Context.instance()
+
+        self.zmq_socket = self.context.socket(zmq.PUSH)
+        self.zmq_socket.connect("inproc://zconf")
+
         try:
             sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # TCP
             #  sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # UDP
@@ -47,6 +53,11 @@ class TCPPoller(threading.Thread):
             if result == 0:
                 if ip not in self.open_address_list:
                     self.open_address_list.append(ip)
+                    self.zmq_socket.send_multipart([b"add", ip.encode('utf-8'), str(port).encode('utf-8')])
+            else:
+                if ip in self.open_address_list:
+                    self.open_address_list.remove(ip)
+                    self.zmq_socket.send_multipart([b"remove", ip.encode('utf-8'), str(port).encode('utf-8')])
             sock.close()
         except:
             pass
@@ -56,13 +67,11 @@ class TCPPoller(threading.Thread):
 
         """
         self.port = port
+        self.context = context
         self.finish = False
         threading.Thread.__init__(self, daemon=True)
         self.max_threads = 50
 
-        self.context = context or zmq.Context.instance()
-        self.zmq_socket = self.context.socket(zmq.PUSH)
-        self.zmq_socket.bind("inproc://tcp_poller")
         self.open_address_list = []
         gws = netifaces.gateways()
         net_dict = netifaces.ifaddresses(gws['default'][netifaces.AF_INET][1])[netifaces.AF_INET]
@@ -73,14 +82,12 @@ class TCPPoller(threading.Thread):
     def run(self):
         while not self.finish:
             for ip in self.network:
-                threading.Thread(target=self.check_port, args=[str(ip), self.port]).start()
+                threading.Thread(target=self.check_port, args=[str(ip), self.port, self.context]).start()
                 #  sleep(0.1)
 
             # limit the number of threads.
             while threading.active_count() > self.max_threads:
                 time.sleep(1)
-            print(self.open_address_list)
-
             time.sleep(60)
 
 
@@ -104,9 +111,7 @@ class tcp_dashBridge(threading.Thread):
     def __on_log(self, client, obj, level, string):
         logging.debug(string)
 
-    def __init__(
-        self, username, password, host='dash.dashio.io', port=8883, context=None
-    ):
+    def __init__(self, username, password, host='dash.dashio.io', port=8883, context=None):
         """
 
         """
@@ -114,26 +119,11 @@ class tcp_dashBridge(threading.Thread):
         threading.Thread.__init__(self, daemon=True)
 
         self.context = context or zmq.Context.instance()
-
-        self.b_connection_id = connection_id.encode('utf-8')
-
-        tx_url_internal = "inproc://TX_{}".format(device_id)
-        rx_url_internal = "inproc://RX_{}".format(device_id)
-
-        self.tx_zmq_pub = self.context.socket(zmq.PUB)
-        self.tx_zmq_pub.connect(tx_url_internal)
-
-        self.rx_zmq_sub = self.context.socket(zmq.SUB)
-        self.rx_zmq_sub.connect(rx_url_internal)
-
-        # Subscribe on ALL, and my connection
-        self.rx_zmq_sub.setsockopt(zmq.SUBSCRIBE, b"ALL")
-        self.rx_zmq_sub.setsockopt(zmq.SUBSCRIBE, b"ANNOUNCE")
-        self.rx_zmq_sub.setsockopt(zmq.SUBSCRIBE, b"ALARM")
-        self.rx_zmq_sub.setsockopt(zmq.SUBSCRIBE, self.b_connection_id)
+        self.rx_zconf_pull = self.context.socket(zmq.PULL)
+        self.rx_zconf_pull.bind("inproc://zconf")
 
         self.poller = zmq.Poller()
-        self.poller.register(self.rx_zmq_sub, zmq.POLLIN)
+        self.poller.register(self.rx_zconf_pull, zmq.POLLIN)
 
         self.LWD = "OFFLINE"
         self.running = True
@@ -156,17 +146,17 @@ class tcp_dashBridge(threading.Thread):
         )
         self.dash_c.tls_insecure_set(False)
 
-        self.control_topic = "{}/{}/control".format(username, device_id)
-        self.data_topic = "{}/{}/data".format(username, device_id)
-        self.alarm_topic = "{}/{}/alarm".format(username, device_id)
-        self.announce_topic = "{}/{}/announce".format(username, device_id)
-        self.dash_c.on_log = self.__on_log
-        self.dash_c.will_set(self.data_topic, self.LWD, qos=1, retain=False)
+        #self.control_topic = "{}/{}/control".format(username, device_id)
+        #self.data_topic = "{}/{}/data".format(username, device_id)
+        #self.alarm_topic = "{}/{}/alarm".format(username, device_id)
+        #self.announce_topic = "{}/{}/announce".format(username, device_id)
+        #self.dash_c.on_log = self.__on_log
+        #self.dash_c.will_set(self.data_topic, self.LWD, qos=1, retain=False)
         # Connect
         self.dash_c.username_pw_set(username, password)
         self.dash_c.connect(host, port)
         # Start subscribe, with QoS level 0
-        self.dash_c.subscribe(self.control_topic, 0)
+        #self.dash_c.subscribe(self.control_topic, 0)
         self.start()
 
     def run(self):
@@ -175,21 +165,22 @@ class tcp_dashBridge(threading.Thread):
         while self.running:
             socks = dict(self.poller.poll())
 
-            if self.rx_zmq_sub in socks:
-                [address, id, data] = self.rx_zmq_sub.recv_multipart()
-                logging.debug("%s TX: %s", self.b_connection_id.decode('utf-8'), data.decode('utf-8').rstrip())
-                if address == b'ANNOUNCE':
-                    self.dash_c.publish(self.announce_topic, data)
-                elif address == b'ALARM':
-                    self.dash_c.publish(self.alarm_topic, data)
-                else:
-                    self.dash_c.publish(self.data_topic, data)
+            if self.rx_zconf_pull in socks:
+                action, ip_address, port = self.rx_zconf_pull.recv_multipart()
+                if action == b'add':
+                    logging.debug("Added device: %s:%s", ip_address.decode('utf-8'), port.decode('utf-8'))
+                    #self.connect_zmq_device(name, ip_address, sub_port, pub_port)
+                elif action == b'remove':
+                    logging.debug("Remove device: %s:%s", ip_address.decode('utf-8'), port.decode('utf-8'))
+                    #try:
+                    #    self.disconnect_zmq_device(name, ip_address, sub_port, pub_port)
+                    #except zmq.error.ZMQError:
+                    #    pass
 
-        self.dash_c.publish(self.announce_topic, "disconnect")
+        #self.dash_c.publish(self.announce_topic, "disconnect")
         self.dash_c.loop_stop()
 
-        self.tx_zmq_pub.close()
-        self.rx_zmq_sub.close()
+        self.rx_zconf_pull.close()
 
 
 def init_logging(logfilename, level):
@@ -234,11 +225,14 @@ def main():
     zeroconf = Zeroconf()
     listener = ZeroConfDashTCPListener(context)
     browser = ServiceBrowser(zeroconf, "_DashTCP._tcp.local.", listener)
-    pinger = TCPPoller()
-    # b = tcp_dashBridge(context=context)
+    pinger = TCPPoller(port=5000, context=context)
+    b = tcp_dashBridge(username='user', password='username', context=context)
 
     while not shutdown:
         time.sleep(5)
+
+    zeroconf.unregister_all_services()
+    zeroconf.close()
 
 
 if __name__ == "__main__":
