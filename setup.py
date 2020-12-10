ebf535608851a80860f408bf71d4e829f1a43399
James Boulton <james@james.boulton@xtra.co.nz>
2020-12-10 17:44:55 +1300

    Fixed typo.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="dashio",
-    version="1.99.3",
+    version="1.99.4",
     # py_modules=['iotcontrol', 'iotconnection'],
     description="Dashio interface library",
     long_description=long_description,
