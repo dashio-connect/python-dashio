10e6fe68b5b9dc899f8fb9244c0e8d7e9259beb2
James Boulton <james@james.boulton@xtra.co.nz>
2020-12-11 16:19:18 +1300

    knobStyle -> style.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="dashio",
-    version="1.99.5",
+    version="1.99.6",
     # py_modules=['iotcontrol', 'iotconnection'],
     description="Dashio interface library",
     long_description=long_description,
