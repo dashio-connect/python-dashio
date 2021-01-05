27b7f4ce10237a3b1b549ddd25942cb3121d5197
James Boulton <james@james.boulton@xtra.co.nz>
2021-01-05 19:01:01 +1300

    update version.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="dashio",
-    version="1.99.94",
+    version="1.99.95",
     # py_modules=['iotcontrol', 'iotconnection'],
     description="Dashio interface library",
     long_description=long_description,
