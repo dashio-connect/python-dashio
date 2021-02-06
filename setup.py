9b5a56b3afc8a52309645b04029c4e677f251850
James Boulton <james@james.boulton@xtra.co.nz>
2021-02-07 12:53:54 +1300

    Bumped version.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="dashio",
-    version="1.99.992",
+    version="1.99.993",
     # py_modules=['iotcontrol', 'iotconnection'],
     description="Dashio interface library",
     long_description=long_description,
