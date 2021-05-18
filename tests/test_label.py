aaa237be63aeff5d069dfbc079e3291e53d2d82a
James Boulton <james.boulton@xtra.co.nz>
2021-05-18 17:16:17 +1200

    More tests.

--- tests/test_label.py
+++ tests/test_label.py
@@ -3,6 +3,7 @@ import unittest
 import json
 from dashio import Label
 
+
 class TestLabel(unittest.TestCase):
     def _get_cfg_dict(self, cfg_str):
         json_str = cfg_str.rpartition('\t')[2]
@@ -10,15 +11,14 @@ class TestLabel(unittest.TestCase):
 
     def test_label_control_type(self):
         test_control = Label("LABELID", color=Color.BLUE)
-        cfg_dict = self._get_cfg_dict(test_control.get_cfg(1,1))
+        cfg_dict = self._get_cfg_dict(test_control.get_cfg(1, 1))
         self.assertEqual(Color(int(cfg_dict['color'])), Color.BLUE, "CFG yAxisMax Should be 1.0")
 
     def test_label_control_id(self):
         test_control = Label("LABELID", style=LabelStyle.BORDER)
-        cfg_dict = self._get_cfg_dict(test_control.get_cfg(1,1))
+        cfg_dict = self._get_cfg_dict(test_control.get_cfg(1, 1))
         self.assertEqual(LabelStyle(cfg_dict['style']), LabelStyle.BORDER, "CFG style Should be 1.0")
-        
-    
-    
+
+
 if __name__ == '__main__':
     unittest.main()
