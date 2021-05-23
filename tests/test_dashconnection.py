a3f64864f94f70f33b261e60afa72967a4e24dae
James Boulton <james@james.boulton@xtra.co.nz>
2021-05-23 13:43:55 +1200

    Fix stuff.

---
+++ tests/test_dashconnection.py
@@ -0,0 +1,19 @@
+import json
+import unittest
+
+from dashio import DashConnection
+from dashio.iotcontrol.enums import Color
+
+
+class TestDashConnection(unittest.TestCase):
+    def _get_cfg_dict(self, cfg_str):
+        json_str = cfg_str.rpartition('\t')[2]
+        return json.loads(json_str)
+
+    def test_dash_connection_username(self):
+        test_connection = DashConnection("USERNAME", "PASSWORD")
+        self.assertEqual(test_connection.username, 'USERNAME', "username type should be USERNAME")
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ tests/test_tcpconnection.py
@@ -0,0 +1,21 @@
+import json
+import unittest
+
+from dashio import TCPConnection
+from dashio.iotcontrol.enums import Color
+
+
+class TestTCPConnection(unittest.TestCase):
+    def _get_cfg_dict(self, cfg_str):
+        json_str = cfg_str.rpartition('\t')[2]
+        return json.loads(json_str)
+
+    def test_TCP_connection_username(self):
+        test_connection = TCPConnection()
+        test_cfg = self._get_cfg_dict(test_connection.tcp_control.get_cfg(1,1))
+        self.assertEqual(test_cfg['ipAddress'], '', "ipAddress should be ''")
+
+
+
+if __name__ == '__main__':
+    unittest.main()
