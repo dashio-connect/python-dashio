a38c754cca9ff21d2a36c313199e98a5de1f9860
James Boulton <james@james.boulton@xtra.co.nz>
2021-06-02 19:41:37 +1200

    Tegged

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="dashio",
-    version="2.1.0",
+    version="2.1.1",
     description="Dashio interface library",
     long_description=long_description,
     long_description_content_type="text/markdown",
