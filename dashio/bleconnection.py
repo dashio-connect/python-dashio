a87e345c60ae4b0ac16200bffd36d149d9bce676
James Boulton <james@james.boulton@xtra.co.nz>
2021-07-10 23:10:41 +1200

    Integrating BLE connection

--- dashio/bleconnection.py
+++ dashio/bleconnection.py
@@ -160,7 +160,8 @@ class BLEConnection(dbus.service.Object, threading.Thread):
         self.watch = GLib.io_add_watch(
             self.rx_zmq_sub.getsockopt(zmq.FD),
             GLib.IO_IN|GLib.IO_ERR|GLib.IO_HUP|GLib.IO_PRI,
-            self.zmq_callback
+            self.zmq_callback,
+            self
         )
 
         self.rx_zmq_sub.setsockopt(zmq.SUBSCRIBE, b"ALL")
