95c8951e2002c13dfbe4ebe7adde0b818d122166
James Boulton <james@james.boulton@xtra.co.nz>
2021-07-23 21:27:03 +1200

    Remove _rx from callbacks.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="dashio",
-    version="2.1.9",
+    version="2.1.10",
     description="Dashio interface library",
     long_description=long_description,
     long_description_content_type="text/markdown",
