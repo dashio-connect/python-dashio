b402b1fd932bb7508a8e455b14897930e5eddf13
James Boulton <james@james.boulton@xtra.co.nz>
2021-09-18 18:54:07 +1200

    fix tests for MapLocation

--- tests/test_map.py
+++ tests/test_map.py
@@ -16,47 +16,47 @@ class TestMapLocation(unittest.TestCase):
 
     def test_map_location_timestamp(self):
         test_location = MapLocation("TAG", -45.237101516008835, 168.84818243505748)
-        test_loc = json.loads(test_location.get_location_data())
+        test_loc = json.loads(str(test_location))
         self.assertIsInstance(dateutil.parser.isoparse(test_loc['time']), datetime, "Should be datetime")
 
     def test_map_location_message(self):
         test_location = MapLocation("TAG", -45.237101516008835, 168.84818243505748)
-        test_loc = json.loads(test_location.get_location_data())
+        test_loc = json.loads(str(test_location))
         self.assertEqual(test_loc['message'], 'TAG', "message should be TAG")
 
     def test_map_location_latitude(self):
         test_location = MapLocation("TAG", -45.237101516008835, 168.84818243505748)
-        test_loc = json.loads(test_location.get_location_data())
+        test_loc = json.loads(str(test_location))
         self.assertEqual(test_loc['latitude'], -45.237101516008835, "latitude should be -45.237101516008835")
 
     def test_map_location_longitude(self):
         test_location = MapLocation("TAG", -45.237101516008835, 168.84818243505748)
-        test_loc = json.loads(test_location.get_location_data())
+        test_loc = json.loads(str(test_location))
         self.assertEqual(test_loc['longitude'], 168.84818243505748, "latitude should be 168.84818243505748")
 
     def test_map_location_avg_speed(self):
         test_location = MapLocation("TAG", -45.237101516008835, 168.84818243505748, average_speed=10)
-        test_loc = json.loads(test_location.get_location_data())
+        test_loc = json.loads(str(test_location))
         self.assertEqual(test_loc['avgeSpeed'], 10, "avgeSpeed should be 10")
 
     def test_map_location_peak_speed(self):
         test_location = MapLocation("TAG", -45.237101516008835, 168.84818243505748, peak_speed=10)
-        test_loc = json.loads(test_location.get_location_data())
+        test_loc = json.loads(str(test_location))
         self.assertEqual(test_loc['peakSpeed'], 10, "peakSpeed should be 10")
 
     def test_map_location_course(self):
         test_location = MapLocation("TAG", -45.237101516008835, 168.84818243505748, course=10)
-        test_loc = json.loads(test_location.get_location_data())
+        test_loc = json.loads(str(test_location))
         self.assertEqual(test_loc['course'], 10, "course should be 10")
 
     def test_map_location_altitude(self):
         test_location = MapLocation("TAG", -45.237101516008835, 168.84818243505748, altitude=10)
-        test_loc = json.loads(test_location.get_location_data())
+        test_loc = json.loads(str(test_location))
         self.assertEqual(test_loc['altitude'], 10, "altitude should be 10")
 
     def test_map_location_distance(self):
         test_location = MapLocation("TAG", -45.237101516008835, 168.84818243505748, distance=10)
-        test_loc = json.loads(test_location.get_location_data())
+        test_loc = json.loads(str(test_location))
         self.assertEqual(test_loc['distance'], 10, "distance should be 10")
 
 
