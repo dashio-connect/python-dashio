1821c59854c1bd75d089c2f88cf7197d004c1c18
James Boulton <james@james.boulton@xtra.co.nz>
2021-09-19 18:56:54 +1200

    Upped version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="dashio",
-    version="2.2.2",
+    version="2.2.3",
     description="DashIO interface library",
     long_description=long_description,
     long_description_content_type="text/markdown",
