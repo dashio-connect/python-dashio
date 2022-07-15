f171f79d1e5972c31e46884795d3ffdf7ed6dd95
James Boulton <james@james.boulton@xtra.co.nz>
2022-07-15 12:33:20 +1200

    Fixed TestEventLog

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="dashio",
-    version="2.2.6",
+    version="2.2.7",
     description="DashIO interface library",
     long_description=long_description,
     long_description_content_type="text/markdown",
