abf67a67d943f4b31c71025e211f5a24706d0de0
James Boulton <james@james.boulton@xtra.co.nz>
2022-11-17 09:24:44 +1300

    Bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="dashio",
-    version="3.1.6",
+    version="3.1.7",
     description="DashIO interface library",
     long_description=long_description,
     long_description_content_type="text/markdown",
