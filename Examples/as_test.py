46e798c190bc242c92c5d69cca8e6773ef6a617e
James Boulton <james@james.boulton@xtra.co.nz>
2022-12-14 20:01:54 +1300

    Add as_test.py

---
+++ Examples/as_test.py
@@ -0,0 +1,111 @@
+"""
+MIT License
+
+Copyright (c) 2020 DashIO-Connect
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+import argparse
+import logging
+import signal
+import time
+import zmq 
+import dashio
+
+class ActionStationOnly:
+    def signal_cntrl_c(self, os_signal, os_frame):
+        self.shutdown = True
+
+    def init_logging(self, logfilename, level):
+        log_level = logging.WARN
+        if level == 1:
+            log_level = logging.INFO
+        elif level == 2:
+            log_level = logging.DEBUG
+        if not logfilename:
+            formatter = logging.Formatter("%(asctime)s, %(message)s")
+            handler = logging.StreamHandler()
+            handler.setFormatter(formatter)
+            logger = logging.getLogger()
+            logger.addHandler(handler)
+            logger.setLevel(log_level)
+        else:
+            logging.basicConfig(
+                filename=logfilename,
+                level=log_level,
+                format="%(asctime)s, %(message)s",
+                datefmt="%Y-%m-%d %H:%M:%S",
+            )
+        logging.info("==== Started ====")
+
+    def parse_commandline_arguments(self):
+        parser = argparse.ArgumentParser()
+        parser.add_argument(
+            "-v",
+            "--verbose",
+            const=1,
+            default=1,
+            type=int,
+            nargs="?",
+            help="""increase verbosity:
+                            0 = only warnings, 1 = info, 2 = debug.
+                            No number means info. Default is no verbosity.""",
+        )
+        parser.add_argument("-u", "--url", help="Host URL.", dest="url", default="tcp://*")
+        parser.add_argument("-d", "--device_id", dest="device_id", default="098876jzdcjh21312", help="IotDashboard Device ID.")
+        parser.add_argument("-p", "--port", dest="port", type=int, default=5650, help="Port number")
+        parser.add_argument(
+            "-n", "--device_name", dest="device_name", default="ActionStationTestt", help="Alias name for device."
+        )
+        parser.add_argument("-l", "--logfile", dest="logfilename", default="", help="logfile location", metavar="FILE")
+        args = parser.parse_args()
+        return args
+
+    def __init__(self):
+
+        # Catch CNTRL-C signel
+        signal.signal(signal.SIGINT, self.signal_cntrl_c)
+        self.shutdown = False
+        args = self.parse_commandline_arguments()
+        self.init_logging(args.logfilename, args.verbose)
+
+        logging.info("    Device ID: %s", args.device_id)
+        logging.info("  Device Name: %s", args.device_name)
+        context = zmq.Context.instance()
+        self.device = dashio.Device("ActionStationTest", args.device_id, args.device_name, add_actions=True, context=context)
+        #self.device.use_cfg64()
+
+        self.tcp_con = dashio.TCPConnection(context=context)
+        self.device.config_revision = 1
+        self.tcp_con.add_device(self.device)
+
+
+        while not self.shutdown:
+            time.sleep(1)
+
+        self.tcp_con.close()
+        self.device.close()
+
+
+def main():
+    tc = ActionStationOnly()
+
+
+if __name__ == "__main__":
+    main()
