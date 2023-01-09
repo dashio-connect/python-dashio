41bf1d0ef6d0f85613a4d3d31e2f4b6a1607ad8b
James Boulton <james@james.boulton@xtra.co.nz>
2023-01-10 10:16:16 +1300

    Start of Examples dir tidy

---
+++ Examples/ex01.py
@@ -0,0 +1,14 @@
+import dashio
+import random
+import time
+
+device = dashio.Device("aDeviceType", "aDeviceID", "aDeviceName")
+tcp_con = dashio.TCPConnection()
+tcp_con.add_device(device)
+first_dial_control = dashio.Dial("FirstDial")
+device.add_control(first_dial_control)
+
+while True:
+    first_dial_control.dial_value = random.random() * 100
+    time.sleep(5)
+    
