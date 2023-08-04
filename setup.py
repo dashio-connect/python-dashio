8b74a59896a14cddeacdf0dd69b87b7be32db3dc
James Boulton <james@james.boulton@xtra.co.nz>
2023-08-05 10:14:33 +1200

    Reaise requires to python3.7

--- setup.py
+++ setup.py
@@ -15,8 +15,8 @@ setup(
     download_url="https://github.com/dashio-connect/python-dashio",
     packages=find_packages(),
     license="MIT",
-    classifiers=["Programming Language :: Python :: 3.6", "Operating System :: OS Independent"],
+    classifiers=["Programming Language :: Python :: 3.7", "Operating System :: OS Independent"],
     install_requires=["paho-mqtt", "pyzmq", "python-dateutil", "zeroconf", "shortuuid", "pyserial", "astral", "pydantic"],
-    python_requires='>3.6.0',
+    python_requires='>3.7.0',
     scripts=['utilities/c64_encode', 'utilities/c64_decode'],
 )
