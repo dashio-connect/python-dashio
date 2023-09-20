7791da655f45c269c5aea41afeba1a285ffd68d6
James Boulton <james@james.boulton@xtra.co.nz>
2023-09-20 15:56:32 +1200

    Bumped version.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="dashio",
-    version="3.4.1",
+    version="3.4.2",
     description="DashIO interface library",
     long_description=long_description,
     long_description_content_type="text/markdown",
