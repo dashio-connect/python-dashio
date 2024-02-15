9a86a53d470e3b5c586c0b37bb5371b70140d9e7
James Boulton <james@dashio.io>
2024-02-15 18:54:45 +1300

    Migrate to paho-mqtt v2.0.0

--- setup.py
+++ setup.py
@@ -16,8 +16,8 @@ setup(
     packages=find_packages(),
     package_data={"dashio": ["py.typed"]},
     license="MIT",
-    classifiers=["Programming Language :: Python :: 3.7", "Operating System :: OS Independent"],
-    install_requires=["paho-mqtt", "pyzmq", "python-dateutil", "zeroconf", "shortuuid", "pyserial", "astral", "pydantic"],
+    classifiers=["Programming Language :: Python :: 3.8", "Operating System :: OS Independent"],
+    install_requires=["paho-mqtt>=2.0.0", "pyzmq", "python-dateutil", "zeroconf", "shortuuid", "pyserial", "astral", "pydantic"],
     python_requires='>3.7.0',
     scripts=['utilities/c64_encode', 'utilities/c64_decode'],
 )
