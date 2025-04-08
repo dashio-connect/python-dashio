92c04a47f93ce50d64488e8d1fbb7db994399721
James Boulton <james@dashio.io>
2025-04-08 17:40:47 +1200

    Add DashIO server data exporter to utilities.

--- setup.py
+++ setup.py
@@ -19,5 +19,5 @@ setup(
     classifiers=["Programming Language :: Python :: 3.8", "Operating System :: OS Independent"],
     install_requires=["paho-mqtt>=2.0.0", "pyzmq", "python-dateutil", "zeroconf", "shortuuid", "pyserial"],
     python_requires='>=3.8.0',
-    scripts=['utilities/c64_encode', 'utilities/c64_decode'],
+    scripts=['utilities/c64_encode', 'utilities/c64_decode', 'utilities/dashio_data_exporter'],
 )
